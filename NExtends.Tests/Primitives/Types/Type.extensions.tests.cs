7beebde28a0615d03c33733745837c34c726f0c0
Nico <nfaugout@lucca.fr>
2017-07-09 22:03:16 +0200

    ADD : test de Lucca à décommenter quand y'aura xUnit dans NExtends

---
+++ NExtends.Tests/Primitives/Types/Type.extensions.tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NExtends.Tests.Primitives.Types
+{
+	public class TypeExtensionsTest
+	{
+		//TODO : passer à xUnit et décommenter ce test
+		//[Fact]
+		//public void IsSubclassOfInterfaceShouldHandleNonGenericInterfaces()
+		//{
+		//	Assert.True(typeof(System.Collections.ArrayList).IsSubclassOfInterface(typeof(System.Collections.IEnumerable)));
+		//}
+	}
+}
