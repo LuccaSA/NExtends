c48077f3ff9b7bb2d1b3701480641ec1a7783dca
Nicolas Faugout <nfaugout@lucca.fr>
2017-11-10 10:32:31 +0100

    MSC : tab vs space

--- NExtends.Tests/StringExtensionsTests.cs
+++ NExtends.Tests/StringExtensionsTests.cs
@@ -8,19 +8,19 @@ using Xunit;
 
 namespace NExtends.Tests
 {
-	public class StringExtensionsTests
-	{
-		[Fact]
-		public void ShouldRemoveDiacritics()
-		{
-			var text = "ÀÁÂÃÄÅàáâãäåÒÓÔÕÖòóôõöÈÉÊËèéêëÌÍÎÏìíîïÙÚÛÜùúûüÿÑñÇç";
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ShouldRemoveDiacritics()
+        {
+            var text = "ÀÁÂÃÄÅàáâãäåÒÓÔÕÖòóôõöÈÉÊËèéêëÌÍÎÏìíîïÙÚÛÜùúûüÿÑñÇç";
 
-			var result = text.RemoveDiacritics();
+            var result = text.RemoveDiacritics();
 
-			var expected = "AAAAAAaaaaaaOOOOOoooooEEEEeeeeIIIIiiiiUUUUuuuuyNnCc";
+            var expected = "AAAAAAaaaaaaOOOOOoooooEEEEeeeeIIIIiiiiUUUUuuuuyNnCc";
 
-			Assert.Equal(expected, result);
-		}
+            Assert.Equal(expected, result);
+        }
 
         [Fact]
         public void ShouldSanitizeSpecialCharacters()
@@ -35,31 +35,31 @@ namespace NExtends.Tests
         }
 
         [Fact]
-		public void ShouldRemoveSpecialCharacters()
-		{
-			var text = @"Jean d'Anois-Dumesnil _[]|./""@{}()";
+        public void ShouldRemoveSpecialCharacters()
+        {
+            var text = @"Jean d'Anois-Dumesnil _[]|./""@{}()";
 
-			var result = text.RemoveSpecialCaracters();
+            var result = text.RemoveSpecialCaracters();
 
-			var expected = "JeandAnoisDumesnil";
+            var expected = "JeandAnoisDumesnil";
 
-			Assert.Equal(expected, result);
-		}
+            Assert.Equal(expected, result);
+        }
 
-		[Theory]
-		[InlineData("toto")]
-		public void ShouldReturnFalseWhenTextIsNotEmail(string value)
-		{
-			Assert.False(value.isEmail());
-		}
+        [Theory]
+        [InlineData("toto")]
+        public void ShouldReturnFalseWhenTextIsNotEmail(string value)
+        {
+            Assert.False(value.isEmail());
+        }
 
-		[Theory]
-		[InlineData("toto@lucca.fr")]
-		[InlineData("toto@l")]
-		[InlineData("toto@lucca")]
-		public void ShouldReturnTrueWhenTextIsEmail(string value)
-		{
-			Assert.True(value.isEmail());
-		}
-	}
+        [Theory]
+        [InlineData("toto@lucca.fr")]
+        [InlineData("toto@l")]
+        [InlineData("toto@lucca")]
+        public void ShouldReturnTrueWhenTextIsEmail(string value)
+        {
+            Assert.True(value.isEmail());
+        }
+    }
 }
