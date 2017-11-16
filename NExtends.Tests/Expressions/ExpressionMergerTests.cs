8d8ea312a0ff43bb7da39a38e4b94998b562c15d
Nicolas Faugout <nfaugout@lucca.fr>
2017-11-16 15:44:54 +0100

    MSC : space vs tab

--- NExtends.Tests/Expressions/ExpressionMergerTests.cs
+++ NExtends.Tests/Expressions/ExpressionMergerTests.cs
@@ -5,47 +5,47 @@ using Xunit;
 
 namespace NExtends.Tests.Expressions
 {
-	interface IUser
-	{
-		int Id { get; }
-	}
+    interface IUser
+    {
+        int Id { get; }
+    }
 
-	class User : IUser
-	{
-		public int Id { get; set; }
-		public User Manager { get; set; }
-	}
+    class User : IUser
+    {
+        public int Id { get; set; }
+        public User Manager { get; set; }
+    }
 
-	class Department
-	{
-		public User Head { get; set; }
-	}
+    class Department
+    {
+        public User Head { get; set; }
+    }
 
-	public class ExpressionMergerTests
-	{
+    public class ExpressionMergerTests
+    {
 
-		[Fact]
-		public void Expressions2Merger()
-		{
-			var user = new User { Id = 2 };
+        [Fact]
+        public void Expressions2Merger()
+        {
+            var user = new User { Id = 2 };
             Expression<Func<User, int>> entryPoint = u => u.Id;
             Expression<Func<int, bool>> expression = i => i == 2;
 
             var result = ExpressionMerger.Merge<User, bool>(entryPoint, expression);
 
-			Assert.True(result.Compile()(user));
-		}
+            Assert.True(result.Compile()(user));
+        }
 
-		[Fact]
-		public void Expressions3Merger()
-		{
-			var dpt = new Department { Head = new User { Id = 2 } };
+        [Fact]
+        public void Expressions3Merger()
+        {
+            var dpt = new Department { Head = new User { Id = 2 } };
             Expression<Func<Department, IUser>> entryPoint = d => d.Head;
             Expression<Func<IUser, bool>> expression = u => u.Id == 2;
 
             var result = ExpressionMerger.Merge<Department, bool>(entryPoint, expression);
 
-			Assert.True(result.Compile()(dpt));
-		}
-	}
+            Assert.True(result.Compile()(dpt));
+        }
+    }
 }
