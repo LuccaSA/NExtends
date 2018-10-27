d4248409553c3d5b1e974ba7040ceb7b79d709b5
Nicolas Faugout <nfaugout@lucca.fr>
2018-10-27 18:57:37 +0200

    ADD Period.cs from RDD

---
+++ NExtends/Primitives/DateTimes/Period.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NExtends.Primitives.DateTimes
+{
+    public struct Period
+    {
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public Period(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
