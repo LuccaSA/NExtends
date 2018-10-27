c855394c674b80841093b7343d7d53d3846585e4
Nicolas Faugout <nfaugout@lucca.fr>
2018-10-27 20:33:40 +0200

    FIX : Raph

--- NExtends/Primitives/DateTimes/Period.cs
+++ NExtends/Primitives/DateTimes/Period.cs
@@ -4,8 +4,8 @@ namespace NExtends.Primitives.DateTimes
 {
     public struct Period
     {
-        public DateTime Start { get; private set; }
-        public DateTime End { get; private set; }
+        public DateTime Start { get; }
+        public DateTime End { get; }
 
         public Period(DateTime start, DateTime end)
         {
