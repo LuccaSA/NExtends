6f4f34a6de6b524854451f94359162acd53298ef
Nicolas Faugout <nfaugout@lucca.fr>
2018-11-02 12:19:15 +0100

    FIX Sonar

--- NExtends/Primitives/DateTimes/NegativeDurationException.cs
+++ NExtends/Primitives/DateTimes/NegativeDurationException.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace NExtends.Primitives.DateTimes
 {
+    [Serializable]
     public class NegativeDurationException : ArgumentException
     {
+        protected NegativeDurationException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
         public NegativeDurationException(string paramName)
             : base("You cannot create or update a period to a negative duration", paramName) { }
     }
