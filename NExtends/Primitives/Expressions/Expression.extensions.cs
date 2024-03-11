4c9f46be2bf1f288f61132d0e4106a57fba6542e
Léna Maquin <lmaquin@luccasoftware.com>
2024-03-11 11:50:11 +0100

    FIX not expression

--- NExtends/Primitives/Expressions/Expression.extensions.cs
+++ NExtends/Primitives/Expressions/Expression.extensions.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Linq;
 using System.Linq.Expressions;
 
 namespace NExtends.Primitives.Expressions
 {
-	public static class ExpressionExtensions
-	{
-		/// <summary>
-		/// http://stackoverflow.com/questions/13705394/how-to-make-a-predicatebuilder-not
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="expr"></param>
-		/// <returns></returns>
-		public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expr)
-		{
-			return Expression.Lambda<Func<T, bool>>
-				(Expression.Not(Expression.Invoke(expr, expr.Parameters.Cast<Expression>())), expr.Parameters);
-		}
-	}
+    public static class ExpressionExtensions
+    {
+        /// <summary>
+        /// http://stackoverflow.com/questions/13705394/how-to-make-a-predicatebuilder-not
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expr"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression)
+            => Expression.Lambda<Func<T, bool>>(Expression.Not(expression.Body), expression.Parameters);
+    }
 }
